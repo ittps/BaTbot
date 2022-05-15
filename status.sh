10c5c48c0b702f95e4b505e76d4febf7b51f85c9
ittp <61617660+ittp@users.noreply.github.com>
2022-05-16 02:08:23 +0300

    Create status.sh

---
+++ status.sh
@@ -0,0 +1,27 @@
+echo "status"
+
+# Uptime
+
+# Network
+
+# 
+
+services=("network system")
+echo $services
+
+
+# network emts status
+
+# network status
+
+# enabled: true
+# interface: igb0
+# speed (upload / download): [54.3/50.2]
+# config: public
+
+
+# proxy config
+# proxy rule add <name> <acl list>
+# proxy access <create/add> <allow/deny> <name> <acl> 
+# proxy status
+# proxy check <acl>
